a919c57b8472dc269b346374dadd186eb0ac2e2a
SughoshBhargav <sughoshsv@gmail.com>
2023-04-21 07:10:07 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js")
+
+
+const app = express();
+
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+const items =['Buy Food','Cook','Eat'];
+const workItems = [];
+
+app.get("/",function(req,res){
+    
+    let day = date.getDay();
+    
+    res.render("list",{
+        listTitle: day, new_item : items
+    });
+});
+
+app.post("/",function(req,res){
+
+    let item = req.body.new_item;
+    if(req.body.list === "Work List"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        item = req.body.new_item;
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work",function(req,res){
+    res.render("list",{listTitle : "Work List",new_item : workItems})
+});
+
+
+app.get("/about", function(req,res){
+    res.render("about");
+});
+
+
+app.listen(3000,function(){
+    console.log("server running on port 3000");
+});
